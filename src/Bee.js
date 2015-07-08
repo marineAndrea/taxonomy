72d34b46c3ea880482bfc9192ec3e20a8ece3af7
marineAndrea <marine.guillanton@gmail.com>
2015-07-08 15:07:33 -0700

    done!

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  // this = Object.create()
+  Grub.apply(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
